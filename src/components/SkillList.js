eef6beefbbbbde7fd807359e96d2b04d23653a1c
Karan09542 <karan09542@gmail.com>
2025-11-23 16:55:57 +0530

    completed

--- src/components/SkillList.js
+++ src/components/SkillList.js
@@ -4,7 +4,7 @@ const SkillList = ({ skills, deleteSkill }) => {
   return (
     <ul id="skill-list">
       {skills.map((skill, idx) => (
-          <li key={idx} id={`skill-number-${idx}`} onClick={deleteSkill}>
+          <li key={idx} id={`skill-number-${idx}`} onClick={() => deleteSkill(idx)}>
             {skill}
           </li>
       ))}
